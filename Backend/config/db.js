2e2a6c8e1c17b95e783361312fcea3abac5957d0
sidra aslam <aslamsidra876@gmail.com>
2026-05-28 13:18:55 +0500

    seventh commit

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,12 +1,16 @@
-import dns from 'node:dns';
+import dns from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected Successfully!"))
-  .catch((err) => console.log("❌ MongoDB connect nahi hua:", err.message));
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("MongoDB Connected Successfully!");
+  } catch (err) {
+    console.log("❌ MongoDB connect nahi hua:", err.message);
+  }
 };
 
 export default connectDB;
